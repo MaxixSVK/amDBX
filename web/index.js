c7475885ab18ff6a3092c4c3eea8b885f800c742
MaxixSK <dev@maxix.sk>
2024-04-22 18:35:01 +0200

    testing for web server

--- web/index.js
+++ web/index.js
@@ -1,17 +1,8 @@
 const express = require('express');
-const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.DB_CONNECTION)
-.then(() => console.log('Connected to MongoDB...'))
-.catch(err => console.error('Could not connect to MongoDB...' + err));
-
 const app = express();
-app.use(express.static('public'));
-
-app.get('/', function(req, res){
-    res.sendFile(__dirname + '/public/index.html');
-});
+app.use(express.static('frontend'));
 
 app.use((req, res, next) => {
     res.status(404).send("404-chan: Page not found");
