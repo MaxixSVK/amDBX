337716f8d51cded3be168e2a10275e0f93ead2e4
Maxix <account@maxix.sk>
2024-05-25 17:13:04 +0200

    Update register username validation

--- src/dataValidation/register.js
+++ src/dataValidation/register.js
@@ -1,39 +1,41 @@
 const Joi = require('joi');
 
 const registerSchema = Joi.object({
-    name: Joi.string()
-      .min(3)
-      .max(30)
-      .required()
-      .messages({
-        'string.base': `Meno by malo byť typu 'text'`,
-        'string.empty': `Meno nemôže byť prázdne pole`,
-        'string.min': `Meno by malo mať minimálnu dĺžku {#limit}`,
-        'string.max': `Meno by malo mať maximálnu dĺžku {#limit}`,
-        'any.required': `Meno je povinné pole`
-      }),
-    email: Joi.string()
-      .email()
-      .required()
-      .messages({
-        'string.base': `Email by mal byť typu 'text'`,
-        'string.email': `Email by mal byť platný email`,
-        'string.empty': `Email nemôže byť prázdne pole`,
-        'any.required': `Email je povinné pole`
-      }),
-    password: Joi.string()
-      .min(8)
-      .required()
-      .messages({
-        'string.base': `Heslo by malo byť typu 'text'`,
-        'string.empty': `Heslo nemôže byť prázdne pole`,
-        'string.min': `Heslo by malo mať minimálnu dĺžku {#limit}`,
-        'any.required': `Heslo je povinné pole`
-      })
+  name: Joi.string()
+    .alphanum()
+    .min(3)
+    .max(18)
+    .required()
+    .messages({
+      'string.base': `Užívateľské meno by malo byť typu 'text'`,
+      'string.alphanum': `Užívateľské meno by malo obsahovať iba alfanumerické znaky`,
+      'string.empty': `Užívateľské meno nemôže byť prázdne pole`,
+      'string.min': `Užívateľské meno by malo mať minimálnu dĺžku {#limit}`,
+      'string.max': `Užívateľské meno by malo mať maximálnu dĺžku {#limit}`,
+      'any.required': `Užívateľské meno je povinné pole`
+    }),
+  email: Joi.string()
+    .email()
+    .required()
+    .messages({
+      'string.base': `Email by mal byť typu 'text'`,
+      'string.email': `Email by mal byť platný email`,
+      'string.empty': `Email nemôže byť prázdne pole`,
+      'any.required': `Email je povinné pole`
+    }),
+  password: Joi.string()
+    .min(8)
+    .required()
+    .messages({
+      'string.base': `Heslo by malo byť typu 'text'`,
+      'string.empty': `Heslo nemôže byť prázdne pole`,
+      'string.min': `Heslo by malo mať minimálnu dĺžku {#limit}`,
+      'any.required': `Heslo je povinné pole`
+    })
 });
 
 async function validateRegister(data) {
-    return await registerSchema.validateAsync(data);
+  return await registerSchema.validateAsync(data);
 }
 
 module.exports = validateRegister;
