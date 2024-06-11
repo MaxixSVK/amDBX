3ff6083453ba2d55e24c0c8e28d7cf334326e1c3
Maxix <account@maxix.sk>
2024-06-11 21:00:42 +0200

    update

--- frontend/js/search.js
+++ frontend/js/search.js
@@ -1,7 +1,8 @@
 let userAnimeListSearch = [];
 let userMangaListSearch = [];
 
-var searchModal = document.getElementsByTagName("search")[0];
+let searchModal = document.getElementsByTagName("search")[0];
+let editEntryModalElement
 
 async function getSearchLists() {
     const [animeList, mangaList] = await Promise.all([
@@ -55,7 +56,23 @@ async function fetchDataSearch(url, options = {}) {
 
 async function editUserEntry(item, type) {
     searchModal.classList.add('hidden');
-    document.body.classList.remove('overflow-hidden')
+    document.body.classList.add('overflow-hidden')
+
+    const response = await fetch(api + `/account`, {
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        }
+    });
+
+    const data = await response.json();
+
+    let anime, manga;
+
+    if (type == 'Anime') {
+        anime = data.anime.find(a => a.id === item._id);
+    } else {
+        manga = data.manga.find(m => m.id === item._id);
+    }
 
     const editEntryModal = document.createElement('div');
     editEntryModal.classList.add('fixed', 'top-0', 'left-0', 'w-full', 'h-full', 'flex', 'items-center', 'justify-center');
@@ -66,24 +83,28 @@ async function editUserEntry(item, type) {
     editEntryModal.appendChild(editEntryBackground);
 
     const editEntryForm = document.createElement('form');
-    editEntryForm.classList.add('bg-white', 'p-6', 'rounded', 'shadow-lg', 'flex', 'flex-col', 'items-start', 'space-y-4', 'w-full', 'h-full', 'md:w-3/4', 'md:h-3/4', 'z-20');
+    editEntryForm.classList.add('bg-white', 'p-6', 'rounded', 'shadow-lg', 'flex', 'flex-col', 'items-start', 'space-y-4', 'w-full', 'h-full', 'md:w-3/4', 'md:h-3/4', 'z-20', 'm-4', 'text-gray-700');
     editEntryForm.id = 'edit-entry-form';
 
     const closeButton = document.createElement('button');
-    closeButton.id = 'close-modal';
+    closeButton.id = 'close-modal-edit-entry';
     closeButton.classList.add('absolute', 'top-2', 'right-2', 'w-10', 'h-10', 'bg-red-500', 'text-white', 'rounded-full', 'p-2', 'focus:outline-none', 'lg:hidden');
     closeButton.textContent = 'X';
     editEntryForm.appendChild(closeButton);
 
     const editEntryTitle = document.createElement('h2');
     editEntryTitle.textContent = item.name;
+    editEntryTitle.classList.add('text-2xl', 'font-bold', 'mb-4');
 
     const editEntryScoreLabel = document.createElement('label');
     editEntryScoreLabel.textContent = 'Hodnotenie';
+    editEntryScoreLabel.classList.add('block', 'text-sm', 'font-medium', 'text-gray-700');
     const editEntryScoreInput = document.createElement('input');
     editEntryScoreInput.type = 'number';
     editEntryScoreInput.min = 0;
     editEntryScoreInput.max = 10;
+    editEntryScoreInput.value = type == 'Anime' ? anime.userRating : manga.userRating;
+
 
     editEntryForm.append(editEntryTitle);
 
@@ -95,17 +116,20 @@ async function editUserEntry(item, type) {
         editEpisodesInput = document.createElement('input');
         editEpisodesInput.type = 'number';
         editEpisodesInput.min = 0;
+        editEpisodesInput.value = anime.userEpisodes;
 
         const editStatusLabel = document.createElement('label');
         editStatusLabel.textContent = 'Status';
         editStatusInput = document.createElement('select');
 
         const statusOptions = ['Watching', 'Completed', 'On Hold', 'Dropped', 'Plan to Watch'];
-
-        statusOptions.forEach(option => {
+        const displayTexts = ['Práve sledujem', 'Dokončené', 'Odložené', 'Dropnuté', 'Plánujem'];
+        
+        statusOptions.forEach((option, index) => {
             const statusOption = document.createElement('option');
             statusOption.value = option;
-            statusOption.textContent = option;
+            statusOption.textContent = displayTexts[index];
+            statusOption.selected = anime.userStatus === option;
             editStatusInput.appendChild(statusOption);
         });
 
@@ -116,6 +140,7 @@ async function editUserEntry(item, type) {
         editChaptersInput = document.createElement('input');
         editChaptersInput.type = 'number';
         editChaptersInput.min = 0;
+        editChaptersInput.value = manga.userChapters;
 
         const editStatusLabel = document.createElement('label');
         editStatusLabel.textContent = 'Status';
@@ -127,6 +152,7 @@ async function editUserEntry(item, type) {
             const statusOption = document.createElement('option');
             statusOption.value = option;
             statusOption.textContent = option;
+            statusOption.selected = manga.userStatus === option;
             editStatusInput.appendChild(statusOption);
         });
 
@@ -134,6 +160,7 @@ async function editUserEntry(item, type) {
     }
 
     const editEntrySubmit = document.createElement('button');
+    editEntrySubmit.classList.add('border', 'border-blue-500', 'hover:border-blue-700', 'text-blue-500', 'hover:text-blue-700', 'py-1', 'px-4', 'rounded');
     editEntrySubmit.type = 'submit';
     editEntrySubmit.textContent = 'Uložiť';
 
@@ -142,10 +169,12 @@ async function editUserEntry(item, type) {
 
     document.body.appendChild(editEntryModal)
 
+    editEntryModalElement = document.getElementById('edit-entry-modal');
+
     editEntryForm.addEventListener('submit', async function (event) {
         event.preventDefault();
         let entryData;
-    
+
         if (type == 'Anime') {
             const userRating = editEntryScoreInput.value;
             const userEpisodes = editEpisodesInput.value;
@@ -157,7 +186,7 @@ async function editUserEntry(item, type) {
             const userStatus = editStatusInput.value;
             entryData = { userRating, userChapters, userStatus };
         }
-    
+
         fetch(api + `/account/${type.toLowerCase()}/update`, {
             method: 'PUT',
             headers: {
@@ -169,19 +198,14 @@ async function editUserEntry(item, type) {
                 ...entryData
             })
         });
-    
+        document.body.classList.remove('overflow-hidden')
         editEntryModal.remove();
     });
 
-    document.getElementById('close-modal').addEventListener('click', function () {
+    document.getElementById('close-modal-edit-entry').addEventListener('click', function () {
+        document.body.classList.remove('overflow-hidden')
         editEntryModal.remove();
     });
-
-    window.onclick = function (event) {
-        if (event.target == document.querySelector('#edit-entry-modal > div:first-child')) {
-            editEntryModal.remove();
-        }
-    }
 }
 
 searchInputSearch.addEventListener('input', async function (event) {
@@ -273,14 +297,17 @@ searchInputSearch.addEventListener('input', async function (event) {
     createSearchResultsSearch('Manga', manga, api + '/account/manga/add');
 });
 
-window.onclick = function (event) {
-    if (event.target == document.querySelector('#search-modal > div:first-child')) {
-        searchModal.classList.add('hidden');
-        document.body.classList.remove('overflow-hidden')
-    }
-}
-
 document.getElementById('close-modal').addEventListener('click', function () {
     searchModal.classList.add('hidden');
     document.body.classList.remove('overflow-hidden')
 });
+
+window.onclick = function (event) {
+    if (event.target == document.querySelector('#search-modal > div:first-child')) {
+        searchModal.classList.add('hidden');
+        document.body.classList.remove('overflow-hidden');
+    } else if (event.target == document.querySelector('#edit-entry-modal > div:first-child')) {
+        document.body.classList.remove('overflow-hidden');
+        editEntryModalElement.remove();
+    }
+}
